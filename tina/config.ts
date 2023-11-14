f451bd55b86a18732165b468d510951b4f8db1e6
site.author <site@novala.com.np>
2023-11-14 23:50:42 +0545

    update tina conf

---
+++ tina/config.ts
@@ -0,0 +1,100 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+// const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "blog",
+        label: "Blogs",
+        path: "content/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "banner",
+            label: "Banner",
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "Categories",
+            list: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "product",
+        label: "Products",
+        path: "content/products",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true
+          },
+          {
+            type: "string",
+            name: "keywords",
+            label: "Keywords",
+            list: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
